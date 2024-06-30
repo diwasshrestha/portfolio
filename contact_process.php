2fac3f96b316d3ede3390a4083c34cf35e968b04
Diwas Shrestha <diwas668@gmail.com>
2024-06-30 18:03:48 +0545

    First Commit

---
+++ contact_process.php
@@ -0,0 +1,47 @@
+<?php
+    $to = "diwas668@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $subject = $_REQUEST['subject'];
+    $number = $_REQUEST['number'];
+    $cmessage = $_REQUEST['message'];
+
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $subject = "You have a message from your Bitmap Photography.";
+
+    $logo = 'img/logo2.png';
+    $link = '#';
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	$body .= "</td></tr></thead><tbody><tr>";
+	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "</tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td></td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+    $send = mail($to, $subject, $body, $headers);
+
+	if ($send)
+    {
+        echo '<script type="text/javascript">alert("Your message was sent!");</script>';
+        echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+
+    }
+    else
+    {
+        echo '<script type="text/javascript">alert("Oops! Your message wasn’t sent. Try again later.");</script>';
+        echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+    }
+?>
